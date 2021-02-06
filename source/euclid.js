69008886aacef339ec87a4e996de3cd2f1dfc9bb
Alekhin Sergey <alekhin1991@gmail.com>
2021-02-06 22:49:48 +0300

    Update JSDoc euclid func

--- source/euclid.js
+++ source/euclid.js
@@ -12,7 +12,7 @@ const nod = (x, y) => x !== 0 ? nod(y % x, x) : y;
 /***
  * Finds gcd of n numbers
  *
- * @param {number[]} numbers - array of numbers
+ * @param {...number} numbers - n numbers
  * @returns {number}
  */
 const euclid = (...numbers) => numbers.reduce((max, current) => nod(max, Math.abs(current)), 0)
